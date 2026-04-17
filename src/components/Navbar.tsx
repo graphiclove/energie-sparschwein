e2f15b347a24cf803a754814deca58a1d8919608
Adam Drobiec <adamdrobiec@MacBookPro.fritz.box>
2026-04-17 14:20:57 +0200

    Use provided alpha logo asset

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -227,7 +227,7 @@ export default function Navbar() {
             aria-label="Wechselbiber Startseite"
           >
             <Image
-              src="/brand/wechselbiber-logo-white-transparent.png"
+              src="/brand/wechselbiber-logo-white.png"
               alt="Wechselbiber"
               width={196}
               height={32}
